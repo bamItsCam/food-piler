bbaa9efa399b665b6313196434a30cbb0680882f
Abigail M. Liff <abigailliff@gmail.com>
2018-01-27 14:33:09 -0500

    Styling preliminary progress for the main page. Navbar.

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -3,12 +3,14 @@ import { Router, Route } from 'react-router';
 import createBrowserHistory from 'history/createBrowserHistory';
 
 import IngredientManager from '../../ui/components/IngredientManager.jsx';
+import Piler from '../../ui/components/Piler.jsx';
 
 const browserHistory = createBrowserHistory();
 
 export const renderRoutes = () => (
   <Router history={browserHistory}>
 	<div>
+      <Route path="/" component={ Piler } />
       <Route path="/admin" component={ IngredientManager } />
     </div>
   </Router>
