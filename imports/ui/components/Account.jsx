c16c73c15b035387068052cb0774165e40f33d17
Abigail Liff <abigailliff@gmail.com>
2018-02-25 01:55:26 -0500

    Account login link styling attempt (does not work yet).

--- imports/ui/components/Account.jsx
+++ imports/ui/components/Account.jsx
@@ -4,6 +4,17 @@ import { Template } from 'meteor/templating';
 import { Blaze } from 'meteor/blaze';
  
 export default class Account extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      styles : {
+        textDecoration:'none',
+        color: 'white',
+        fontWeight:'bold'
+        }
+      };
+  }
+
   componentDidMount() {
     // Use Meteor Blaze to render login buttons
     this.view = Blaze.render(Template.loginButtons,
@@ -13,8 +24,10 @@ export default class Account extends Component {
     // Clean up Blaze view
     Blaze.remove(this.view);
   }
+
+
   render() {
     // Just render a placeholder container that will be filled in
-    return <span ref="container" />;
+    return (<span style={this.state.styles}ref="container"/>);
   }
 }
